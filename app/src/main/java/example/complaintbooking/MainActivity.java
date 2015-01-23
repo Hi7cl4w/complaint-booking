c7f71c5ad647400758d9df72d1cb796e39ed50ff
unknown <iamanjithsasindran@gmail.com>
2015-01-24 01:01:30 +0530

    Completed Login UI

---
+++ app/src/main/java/example/complaintbooking/MainActivity.java
@@ -0,0 +1,21 @@
+package example.complaintbooking;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_awesome_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
